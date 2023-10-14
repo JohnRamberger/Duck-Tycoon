8991d1f2c8b113f7c582974dd61c3fa7282333b8
Richard Zhang <105668049+RichardZhangRZ@users.noreply.github.com>
2023-10-14 13:47:15 -0400

    Created dummy users endpoint (#6)

--- server/main.py
+++ server/main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+import os
+from flask import Blueprint, Flask
+from routes.users_router import users_route_blueprint
 
 app = Flask(__name__)
+root_blueprint = Blueprint("API root", import_name=__name__, url_prefix="/api")
 
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+root_blueprint.register_blueprint(users_route_blueprint)
+app.register_blueprint(root_blueprint)
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
