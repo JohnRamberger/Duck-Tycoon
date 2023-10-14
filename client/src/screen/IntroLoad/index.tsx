5a06cf7be584f6517cdd2dbed23ba070b86f3b5b
JohnRamberger <jramberger3@gatech.edu>
2023-10-14 19:10:50 -0400

    Create routing & stats components

--- client/src/screen/IntroLoad/index.tsx
+++ client/src/screen/IntroLoad/index.tsx
@@ -1,4 +1,5 @@
 import styles from './index.module.scss';
+import globalstyles from '../../global.module.scss';
 
 import { Flex, Spin } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
@@ -6,12 +7,10 @@ import { LoadingOutlined } from '@ant-design/icons';
 const antIcon = <LoadingOutlined style={{ fontSize: 64 }} spin />;
 
 export const IntroLoadScreen: React.FC = () => (
-  <div className={styles.IntroLoad}>
-    <Flex vertical justify="center" style={{ height: '100%' }}>
-      <Flex vertical>
-        <Spin indicator={antIcon} />
-        <h1>Loading...</h1>
-      </Flex>
+  <div className={`${styles.IntroLoad} ${globalstyles.Center}`}>
+    <Flex vertical>
+      <Spin indicator={antIcon} />
+      <h1>Loading...</h1>
     </Flex>
   </div>
 );
