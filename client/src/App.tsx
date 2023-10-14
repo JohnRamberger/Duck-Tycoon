6140265cd0932f0ec95db481be90e974ebc2a8fa
JohnRamberger <jramberger3@gatech.edu>
2023-10-14 14:34:54 -0400

    Setup Firebase auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,44 @@
 import './App.css';
 
-import { ConfigProvider, Space, theme } from 'antd';
+import { useState, useEffect } from 'react';
+import { ConfigProvider, Space, theme, message } from 'antd';
 
 import { HomeScreen } from './screen/Home';
-// import { IntroLoadScreen } from './screen/IntroLoad';
-// import { LoginScreen } from './screen/Login';
+import { IntroLoadScreen } from './screen/IntroLoad';
+import { LoginScreen } from './screen/Login';
+
+import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 
 const App = () => {
+  // check if user is logged in
+  const [messageApi, contextHolder] = message.useMessage();
+  const [loggedIn, setLoggedIn] = useState<boolean | undefined>();
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if (loggedIn === undefined && user) {
+        messageApi.open({
+          type: 'success',
+          content: 'Logged in!',
+          duration: 3,
+        });
+      }
+      if (loggedIn && user == null) {
+        messageApi.open({
+          type: 'success',
+          content: 'Signed out!',
+          duration: 3,
+        });
+      }
+      setLoggedIn(user != null);
+
+      return () => {
+        unsub();
+      };
+    });
+  }, [messageApi, loggedIn]);
+
   return (
     <div className="App">
       <ConfigProvider
@@ -18,9 +50,15 @@ const App = () => {
         }}
       >
         <Space>
-          {/* <IntroLoadScreen /> */}
-          {/* <LoginScreen /> */}
-          <HomeScreen />
+          {contextHolder}
+
+          {loggedIn === undefined && <IntroLoadScreen />}
+          {loggedIn !== undefined && (
+            <>
+              {!loggedIn && <LoginScreen />}
+              {loggedIn && <HomeScreen />}
+            </>
+          )}
         </Space>
       </ConfigProvider>
     </div>
