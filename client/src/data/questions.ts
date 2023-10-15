4bdab4922bb3c5713806feb78b5d3754c3227ed0
JohnRamberger <jramberger3@gatech.edu>
2023-10-14 22:31:07 -0400

    Create questions.ts

---
+++ client/src/data/questions.ts
@@ -0,0 +1,29 @@
+interface MCQ {
+  question: string;
+  answer: string;
+  fakeAnswers: string[];
+}
+
+export const SCHOOL_QUESTIONS: MCQ[] = [
+  {
+    question: '',
+    answer: '',
+    fakeAnswers: ['', '', ''],
+  },
+];
+
+export const WORK_QUESTIONS1: MCQ[] = [
+  {
+    question: '',
+    answer: '',
+    fakeAnswers: ['', '', ''],
+  },
+];
+
+export const WORK_QUESTIONS2: MCQ[] = [
+  {
+    question: '',
+    answer: '',
+    fakeAnswers: ['', '', ''],
+  },
+];
