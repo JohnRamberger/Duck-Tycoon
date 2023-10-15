042610e20952ed40a50183c4420c1ded2d3cedf2
rzhang630 <rzhang630@gatech.edu>
2023-10-14 21:55:47 -0400

    Added create user

--- server/routes/users_router.py
+++ server/routes/users_router.py
@@ -1,15 +1,31 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 from services.users_service import (
+    create_user,
     get_bank_account_of_user,
     get_stocks_of_user,
     get_users,
     get_user,
 )
+from psycopg2.errors import UniqueViolation
 
 users_route_blueprint = Blueprint("users", __name__, url_prefix="/user")
 
 
-@users_route_blueprint.route("/")
+@users_route_blueprint.route("/", methods=["POST"])
+def create_new_user():
+    body_data = request.json
+    if "username" not in body_data:
+        return jsonify({"error": "No username provided"}), 400
+    if "userid" not in body_data:
+        return jsonify({"error": "No userid provided"}), 400
+    try:
+        create_user(body_data["username"], body_data["userid"])
+    except UniqueViolation:
+        return {"error": "userid already taken"}
+    return {}, 201
+
+
+@users_route_blueprint.route("/", methods=["GET"])
 def fetch_list_users():
     user_list = get_users()
     return jsonify({"users": user_list})
