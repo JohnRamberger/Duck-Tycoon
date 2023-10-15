2ccf3fab4f3518c2728f616be3463145b05a3fc2
John Ramberger <jramberger3@gatech.edu>
2023-10-15 03:40:39 -0400

    Initial API Integration (#14)
    * Initial API Integration
    
    * Update index.tsx

---
+++ client/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://duck-tycoon-api-p3mlfcmlha-ul.a.run.app',
+      changeOrigin: true,
+    })
+  );
+};
